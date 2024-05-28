a120626d577733ab4a904e001b7e618a192fc211
paritagala <paritagala29@gmail.com>
2024-05-27 20:55:28 -0500

    First Step of the technical challenge

---
+++ 1.Read_File_Api.py
@@ -0,0 +1,25 @@
+#Step 1 : Pip install requests in the terminal
+#Step 2: Import requests below
+
+import requests
+
+#Step 3: Define the url of the API : in other words give a name to the API
+
+url = "https://earthquake.usgs.gov/fdsnws/event/1/application.json"
+
+#Step 4: Find the data from the API. This is when I used the comman "request get"
+
+response = requests.get(url)
+
+#Step 5: Checking if the request was successful it will return the status code 200
+if response.status_code == 200:
+    # Step 6: If the status code is 200 the parse the json response
+    data = response.json()
+    #Step 7: And print data
+    print(data)
+
+#Step 8: Else print failed to find the data
+else: 
+    print(f"Failed to find data, status code:{response.status_code}")
+
+
